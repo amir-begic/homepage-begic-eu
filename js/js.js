5b6920ef88aff65e8bf82d74ccfa370b6d2f76f1
Amir Begic <amir.begic@namics.com>
2020-05-30 01:09:27 +0200

    added auto rotating carousel

--- js/js.js
+++ js/js.js
@@ -1,11 +1,13 @@
-
 function draw() {
+    
+    requestAnimationFrame(draw)
     ctx.fillStyle = '#004494';
     ctx.fillRect(0, 0, window.innerWidth, window.innerHeight); 
+
+    drawBoxes()
     drawLines()
-    //drawStar()
-    drawText()
-    requestAnimationFrame(draw)
+    spinBoxes()
+
 }
 
 function drawLines(){
@@ -13,41 +15,69 @@ function drawLines(){
     let sectionCount = 5;
     let linedistanceX = 100 / sectionCount;
     let linepositionX = linedistanceX;
-    ctx.beginPath();
 
-    for(i = 0; i < sectionCount ;i++){
+    ctx.beginPath();
+    ctx.lineWidth = "2";
+    ctx.strokeStyle = "white";
+
+    for (i = 0; i < sectionCount ;i++){
         ctx.moveTo(onePercentX * linepositionX, 0);
-        ctx.lineWidth = "2";
-        ctx.strokeStyle = "white";
         ctx.lineTo(onePercentX * linepositionX, window.innerHeight);
         linepositionX += linedistanceX;
     }
     ctx.stroke();
 }
 
-function drawText(){
-    let initialPosition = 200
 
-    ctx.font = "80px Arial";
-    ctx.fillStyle = "white"
-    if (initial){
-        ctx.fillText("Hello World", 300, 200 + (offset1 / 2));
+function drawBoxes(){
+    let onePercentX = window.innerWidth/100;
+    let boxCount = 5;
+    let xPos = 0;
+    let xOffset = 100 / boxCount;
+    let boxHeight = window.innerHeight/32;
+    let boxWidth = window.innerWidth/5;
+
+    ctx.fillStyle = '#aeae93';
+    ctx.font = "20px Arial";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    padding = 2;
+
+    for (i=0; i < boxCount;i++){
+        let x = onePercentX*xPos;
+        let y = offsetY[i];
+
+        ctx.fillStyle = '#aeae93';
+        ctx.fillRect(x, y, boxWidth, boxHeight);
+        
+        ctx.fillStyle = 'white';        
+        ctx.fillText(textElements[i], x + boxWidth / 2 +  padding, y + padding + boxHeight/ 2);
+        xPos += xOffset;
     }
+}
 
-    if (offset1 > -20 && !initial){
-        //console.log(1)
-        ctx.fillText("Hello World", 300, 0 + (offset1 / 2));
+function spinBoxes(){
+    for (i = 0; i < 5; i++){ 
+        TweenMax.to(tweenElement[i], tweenDuration[i], {y: window.innerHeight * 7.5, onComplete: drawStars });
+        offsetY[i] = tweenElement[i].y > (window.innerHeight*Math.floor(tweenElement[i].y/window.innerHeight)) ? 
+            tweenElement[i].y - (window.innerHeight*Math.floor(tweenElement[i].y/window.innerHeight)) : tweenElement[i].y
     }
-    // if (offset2 > -20 && !initial){
-    //     //console.log(2)
-    //     ctx.fillText("Hello World", 300, 0 + offset2);
-    // }
-
+    
 
 }
-var initial = true
-var offset1 = 0
-var offset2 = 0
+
+function drawStars(){
+    console.log("go")
+}
+
+var reset = false;
+var textElements = ['Amir', 'Begic', 'Starcraft', 'Warhammer', 'Civilization'];
+var initialOffset = 0;
+var tweenDuration = [3, 4, 5.4, 5, 3.6];
+var offsetY = [initialOffset, initialOffset, initialOffset, initialOffset, initialOffset];
+var offsetYtween = {y: 0};
+var tweenElement = [{y:0},{y:0},{y:0},{y:0},{y:0}];
+
 var canvas = document.querySelector('#canvas');
 canvas.style.width = window.innerWidth;
 canvas.width= window.innerWidth;
@@ -56,39 +86,11 @@ canvas.style.height = window.innerHeight;
 if (canvas.getContext) {
     var ctx = canvas.getContext('2d');
 }
-
 draw()
-
-this.canvas.addEventListener('wheel',function(event){
-    //mouseController.wheel(event);
-    offset1 += event.deltaY    
-    offset2 += event.deltaY
-
-
-    if (offset1 > canvas.height + 60 )  
-    {
-        offset1 = 0;
-        initial = false;
-    }
-    if (offset1 < 0){
-        
-        offset1 = canvas.height - 20;
-    }
-
-    if (offset1 < 0){
-        
-        offset1 = canvas.height - 20;
-    }
-
-    
-    console.log(offset1)
-    return false; 
-}, false)
-
+  
 window.addEventListener('resize', ()=>{
     canvas.style.width = window.innerWidth;
     canvas.width= window.innerWidth;
     canvas.height= window.innerHeight;
     canvas.style.height = window.innerHeight;
 })
-
